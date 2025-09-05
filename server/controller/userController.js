bc4c7ea2472b94d13cab2a52aa7b01c0cec5627e
lynx-fx <anupbhujel07@gmail.com>
2025-09-05 09:40:36 +0545

    Cleaned userController

--- server/controller/userController.js
+++ server/controller/userController.js
@@ -33,35 +33,9 @@ exports.getUserDetails = async (req, res) => {
   }
 };
 
-exports.sendFriendRequest = async (req, res) => {
+exports.handleFriends = async (req, res) => {
   try {
-    const { friendId } = req.body;
-
-    const token = tokenExtractor(req);
-    if (!token) {
-      return res
-        .status(400)
-        .json({ success: false, message: "Token not provided" });
-    }
-
-    const decode = jwt.verify(token, process.env.JWT_SECRET);
-    const friend = await User.findById(friendId);
-
-    friend.friendRequests.push(decode.id);
-    await friend.save();
-
-  } catch (err) {
-    console.log(err);
-    return res.status(500).json({
-      success: false,
-      message: "Something went wrong while sending request",
-    });
-  }
-};
-
-exports.acceptFriendRequest = async (req, res) => {
-  try {
-    const { friendId } = req.body;
+    const { friendId, action } = req.body;
 
     const token = tokenExtractor(req);
     if (!token) {
@@ -85,119 +59,83 @@ exports.acceptFriendRequest = async (req, res) => {
         .json({ success: false, message: "Friend not found" });
     }
 
-    // updating user's friend list
-    await User.findByIdAndUpdate(user._id, {
-      $pull: { friendRequests: friend._id },
-      $addToSet: { friends: friend._id },
-    });
+    if (action == "accept") {
+      // updating user's friend list
+      await User.findByIdAndUpdate(user._id, {
+        $pull: { friendRequests: friend._id },
+        $addToSet: { friends: friend._id },
+      });
 
-    // updating friend's friend list
-    await User.findByIdAndUpdate(friend._id, {
-      $addToSet: { friends: user._id },
-    });
+      // updating friend's friend list
+      await User.findByIdAndUpdate(friend._id, {
+        $addToSet: { friends: user._id },
+      });
 
-    // gets updated friend's list to refresh list
-    const updatedFriends = await User.findById(user._id).populate(
-      "friends",
-      "username email profileURI isOnline"
-    );
+      // gets updated friend's list to refresh list
+      const updatedFriends = await User.findById(user._id).populate(
+        "friends",
+        "username email profileURI isOnline"
+      );
 
-    return res.status(200).json({
-      success: true,
-      message: "Friend request accepted",
-      friends: updatedFriends,
-    });
-  } catch (err) {
-    console.log(err);
-    return res.status(500).json({
-      success: false,
-      message: "Something went wrong while accepting request",
-    });
-  }
-};
+      return res.status(200).json({
+        success: true,
+        message: "Friend request accepted",
+        friends: updatedFriends,
+      });
+    } else if (action == "reject") {
+      await User.findByIdAndUpdate(friend._id, {
+        $addToSet: { friendRequests: user._id },
+      });
 
-exports.removeFriend = async (req, res) => {
-  try {
-    const { friendId } = req.body;
+      const updatedFriendRequests = await User.findById(user._id).populate(
+        "friendRequests",
+        "username email profileURI"
+      );
 
-    const token = tokenExtractor(req);
-    if (!token) {
+      return res.status(200).json({
+        success: true,
+        message: "Friend request rejected successfully",
+        updatedFriendRequests,
+      });
+    } else if (action == "send") {
+      friend.friendRequests.push(decode.id);
+      await friend.save();
+
+      return res
+        .status(200)
+        .json({ success: true, message: "Friend request sent successfully" });
+    } else if (action == "remove") {
+      // updating user's friend list
+      await User.findByIdAndUpdate(user._id, {
+        $pull: { friends: friend._id },
+      });
+
+      // updating friend's friend list
+      await User.findByIdAndUpdate(friend._id, {
+        $pull: { friends: user._id },
+      });
+
+      // gets updated friend's list to refresh list
+      const updatedFriends = await User.findById(user._id).populate(
+        "friends",
+        "username email profileURI isOnline"
+      );
+
+      return res.status(200).json({
+        success: true,
+        message: "Friend removed successfully",
+        friends: updatedFriends,
+      });
+    } else {
       return res
         .status(400)
-        .json({ success: false, message: "No token provided" });
+        .json({ success: false, message: "Action not provided" });
     }
-
-    const decode = jwt.verify(token, process.env.JWT_SECRET);
-    const user = await User.findOne({ email: decode.email });
-    if (!user) {
-      return res
-        .status(404)
-        .json({ success: false, message: "User not found" });
-    }
-
-    const friend = await User.findById(friendId);
-    if (!friend) {
-      return res
-        .status(404)
-        .json({ success: false, message: "Friend not found" });
-    }
-
-    // updating user's friend list
-    await User.findByIdAndUpdate(user._id, {
-      $pull: { friends: friend._id },
-    });
-
-    // updating friend's friend list
-    await User.findByIdAndUpdate(friend._id, {
-      $pull: { friends: user._id },
-    });
-
-    // gets updated friend's list to refresh list
-    const updatedFriends = await User.findById(user._id).populate(
-      "friends",
-      "username email profileURI isOnline"
-    );
-
-    return res.status(200).json({
-      success: true,
-      message: "Friend request accepted",
-      friends: updatedFriends,
-    });
   } catch (err) {
     console.log(err);
     return res.status(500).json({
       success: false,
-      message: "Something went wrong while accepting request",
-    });
-  }
-};
-
-exports.rejectFriendRequest = async (req, res) => {
-  try {
-    const { friendId } = req.body;
-
-    const token = tokenExtractor(req);
-    if (!token) {
-      return res
-        .status(400)
-        .json({ success: false, message: "No token provided" });
-    }
-
-    const decode = jwt.verify(token, process.env.JWT_SECRET);
-    const user = await User.findOne({ email: decode.email });
-    if (!user) {
-      return res
-        .status(404)
-        .json({ success: false, message: "User not found" });
-    }
-
-    await user.friendRequests.pull(friendId);
-    await user.save();
-  } catch (err) {
-    console.log(err);
-    return res.status(500).json({
-      success: false,
-      message: "Something went wrong while rejecting friend request",
+      message: "Something went wrong while handling friend request",
     });
   }
 };
